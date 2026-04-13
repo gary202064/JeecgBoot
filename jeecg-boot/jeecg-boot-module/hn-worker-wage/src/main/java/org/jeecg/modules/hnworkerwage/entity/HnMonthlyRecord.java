8ffecde5d6059e3fd9277aa944543deb995db173
gary <gun54@163.com>
2026-04-13 09:10:54 +0800

    修复保存报错

--- jeecg-boot/jeecg-boot-module/hn-worker-wage/src/main/java/org/jeecg/modules/hnworkerwage/entity/HnMonthlyRecord.java
+++ jeecg-boot/jeecg-boot-module/hn-worker-wage/src/main/java/org/jeecg/modules/hnworkerwage/entity/HnMonthlyRecord.java
@@ -3,6 +3,7 @@ package org.jeecg.modules.hnworkerwage.entity;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
